b32ff94d155ee32cd60f6c9525912fad7b864846
Eusebiu.Jacot <sebi1.77@mail.ru>
2023-12-15 15:24:22 +0100

    last

--- src/main/java/com/example/onlinebankingfinal/aspect/LoggingAspect.java
+++ src/main/java/com/example/onlinebankingfinal/aspect/LoggingAspect.java
@@ -10,6 +10,56 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
+/**
+ * The LoggingAspect class is responsible for logging requests and method execution in the controller and service layers.
+ * It uses the AspectJ annotation @Aspect to define the aspect and @Component to make it a Spring bean.
+ * This class uses the Lombok annotation @Slf4j to enable logging using the Self Logging Facade for Java (SLF4J) framework.
+ *
+ * <p>The LoggingAspect class contains the following methods:</p>
+ *
+ * <ul>
+ *   <li>controllerLog() - Defines the pointcut expression for logging controller methods.</li>
+ *   <li>serviceLog() - Defines the pointcut expression for logging service methods.</li>
+ *   <li>doBeforeController() - Logs information before executing a controller method.</li>
+ *   <li>doBeforeService() - Logs information before executing a service method.</li>
+ *   <li>doAfterReturning() - Logs the return value after executing a controller method.</li>
+ *   <li>throwsException() - Logs an exception when a controller method throws an exception.</li>
+ * </ul>
+ *
+ * <p>This class makes use of the following dependencies:</p>
+ *
+ * <ul>
+ *   <li>ServletRequestAttributes - Used to get the servlet request attributes.</li>
+ *   <li>HttpServletRequest - Represents an HTTP request.</li>
+ *   <li>RequestContextHolder - Provides access to the current request.</li>
+ *   <li>JoinPoint - Represents a point during the execution of a program.</li>
+ * </ul>
+ *
+ * <p>Example usage:</p>
+ *
+ * <pre><code>
+ *     // Create an instance of the LoggingAspect class
+ *     LoggingAspect loggingAspect = new LoggingAspect();
+ *
+ *     // Execute controllerLog() method
+ *     loggingAspect.controllerLog();
+ *
+ *     // Execute serviceLog() method
+ *     loggingAspect.serviceLog();
+ *
+ *     // Execute doBeforeController() method
+ *     loggingAspect.doBeforeController(jp);
+ *
+ *     // Execute doBeforeService() method
+ *     loggingAspect.doBeforeService(jp);
+ *
+ *     // Execute doAfterReturning() method
+ *     loggingAspect.doAfterReturning(returnObject);
+ *
+ *     // Execute throwsException() method
+ *     loggingAspect.throwsException(jp, ex);
+ * </code></pre>
+ */
 @Aspect
 @Component
 @Slf4j //SELF LOGGING FACADE FOR JAVA
